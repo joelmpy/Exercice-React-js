50a35daaa334ad7dc63bc0becc41612c6a4d2c36
Joel <joel_mpy@hotmail.com>
2022-07-09 21:42:33 +0200

    first commit

---
+++ jour-01/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Bonjour</h2>
+     <h1 className="text-primary"> Hello !</h1>
+    </div>
+  );
+}
+
+export default App;
