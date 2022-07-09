dc8fb183a07ff8d2b33fbc84344796e99c1f52c1
Joel <joel_mpy@hotmail.com>
2022-07-09 21:52:42 +0200

    first commit

--- jour-01/src/App.js
+++ jour-01/src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
 
 function App() {
+
+  const temMin = -20;
+  const tempMax = 40;
+  const heartMin = 80;
+  const heartMax = 180;
+  const stepsMin = 0;
+  const stepsMax = 50000
   return (
-    <div className="App">
+    <div className="container-fluid">
+      <div className="row">
+        <p>Temperature :  {temMin}</p>
+        <p>{tempMax}</p>
+        <p> Heart :{heartMin}</p>
+        <p>{heartMax}</p>
+        <p>Steps : {stepsMin}</p>
+        <p>{stepsMax}</p>
+      </div>
      <h2>Bonjour</h2>
      <h1 className="text-primary"> Hello !</h1>
     </div>
